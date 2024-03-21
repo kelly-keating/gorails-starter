18913b166f5dde6870dcbfe78fb20d3d62c64419
kelly-keating <keating.kel@gmail.com>
2024-03-21 16:07:32 +1300

    add validation to migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,21 @@
 # email: string
-
-# v-- we don't touch this hash ourselves
 # password_digest: string
 
 #  v-- not stored in db, but used by bcrypt to confirm and then make digest
 # password: string virtual
 # password_confirmation: string virtual
 
+# rails c
+# user = User.create({email: "kelly", password: 'hi', password_confirmation: "hi" })
+# user.errors.any?
+
+# rails db:migrate
+# rails db:rollback
+# rails db:migrate:redo    <-- is the combo rollback & latest
+
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email" }
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'invalid email address provided' }
 end
