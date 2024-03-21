1bb112d1fe59605fda1718261d2cf946fd73fc90
kelly-keating <keating.kel@gmail.com>
2024-03-21 15:46:02 +1300

    add bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
+# email: string
+
+# v-- we don't touch this hash ourselves
+# password_digest: string
+
+#  v-- not stored in db, but used by bcrypt to confirm and then make digest
+# password: string virtual
+# password_confirmation: string virtual
+
 class User < ApplicationRecord
+  has_secure_password
 end
