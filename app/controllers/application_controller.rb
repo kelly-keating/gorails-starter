c36d79c402f830e72a364af7124239707c499966
kelly-keating <keating.kel@gmail.com>
2024-03-26 14:06:41 +1300

    add Current model to fix user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      # @user = User.find(session[:user_id]) <-- throws error
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 end
